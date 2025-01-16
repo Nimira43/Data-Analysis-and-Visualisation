9e91d5dc7f5d5a47ababb47bfd79c0e5efc4d16c
Nimira43 <n.rabone72@aol.com>
2025-01-16 05:16:27 +0000

    Dataset shap and info

--- numerical-relationships.py
+++ numerical-relationships.py
@@ -1,3 +1,7 @@
 import pandas
 housing_data = pandas.read_csv('./housing_data.csv')
 print(housing_data)
+print('---------------')
+print(housing_data.shape) # (1460, 81)
+print('---------------')
+print(housing_data.info())
